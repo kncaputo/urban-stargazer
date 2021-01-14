ccf8f77b716924329979fc4f206d6d1cd16827bd
Kara Caputo <kncaputo@gmail.com>
2021-01-13 19:09:55 -0500

    Add header component

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+console.log(process.env)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <main>
+        <Header />
+
+      </main>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Urban Stargazer</h1>
+      <h2>Saved</h2>
+    </header>
+  )
+}
