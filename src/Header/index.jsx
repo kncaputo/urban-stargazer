4b6a9c612c2cd1f9d861f6d879a1ae42793d5d3d
Kara Caputo <kncaputo@gmail.com>
2021-01-15 21:53:42 -0500

    Add header fonts

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -4,13 +4,17 @@ import './Header.scss';
 const Header = () => {
   return(
     <header>
-      <h1>Urban Stargazer</h1>
+
+      <Link to='/home' className='nav-link' id='page-title-link'>
+        <h1>Urban Stargazer</h1>
+      </Link>
+
       <nav>
-        <Link to='/discover'>
-          Discover
+        <Link to='/discover' className='nav-link'>
+          <h2>Discover</h2>
         </Link>
-        <NavLink to='saved'>
-          Saved
+        <NavLink to='/saved' className='nav-link'>
+          <h2>Saved</h2>
         </NavLink>
       </nav>
     </header>
