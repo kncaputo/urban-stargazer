2abdc051c1f1f59b0924af9abefcfb364e893915
Kara Caputo <kncaputo@gmail.com>
2021-01-17 13:38:43 -0500

    Add react-icons package and context for styles

--- src/SavedModal/index.jsx
+++ src/SavedModal/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai'
+import { IconContext } from 'react-icons/lib';
 import './SavedModal.scss';
 
 const SavedModal = (props) => {
@@ -7,21 +9,24 @@ const SavedModal = (props) => {
   const { closeModal } = props.closeModal;
 
   return (
-    <section id='saved-modal-box'>
+    <IconContext.Provider value={{ color: 'white'}}>
+      <section id='saved-modal-box'>
 
-      <section id='modal-button-box'>
-        <button className='modal-button' onClick={deleteSavedImage}>Remove from Saved</button>
-        <button className='modal-button' onClick={closeModal}>Close</button>
-      </section>
+        <section id='modal-button-box'>
+          <button className='modal-button' onClick={deleteSavedImage}>Remove from Saved</button>
+          <AiOutlineClose className='modal-button' onClick={closeModal}/>
+          {/* <button className='modal-button' onClick={closeModal}><AiOutlineClose onClick={closeModal}/></button> */}
+        </section>
 
-      <section>
-        <img id='modal-image' src={`${url}`} alt={`${title}`}/>
-      </section>
-      <section>
-        <h4 id='modal-title'>{title}</h4>
+        <section>
+          <img id='modal-image' src={`${url}`} alt={`${title}`}/>
+        </section>
+        <section>
+          <h4 id='modal-title'>{title}</h4>
 
-      </section>
-    </section>  
+        </section>
+      </section>  
+    </IconContext.Provider>
   ) 
 }
 
