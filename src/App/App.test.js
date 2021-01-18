e7ff9577dc7ea4b66ebd2311bcf6e493b5c54cba
Kara Caputo <kncaputo@gmail.com>
2021-01-18 08:43:31 -0500

    Complete pathname tests for Header

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,3 +1,3 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { act, render, screen, fireEvent } from '@testing-library/react';
+// import React from 'react';
 import Header from './index.jsx';
+import { act, render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe('Header', () => {
+  const history = createMemoryHistory();
+
   beforeEach(() => {
     render(
-      <Router>
+      <Router history={history} > 
         <Header />
       </Router>  
     );
@@ -20,5 +25,29 @@ describe('Header', () => {
     expect(home).toBeInTheDocument();
     expect(discoverNav).toBeInTheDocument();
     expect(savedNav).toBeInTheDocument();
+  });
+
+  it('should link to home when \'Urban Stargazer\' is clicked', () => {
+    const home = screen.getByText('Urban Stargazer');
+    
+    userEvent.click(home);
+
+    expect(history.location.pathname).toBe('/home');
+  });
+
+  it('should link to home when \'Discover\' is clicked', () => {
+    const discoverNav = screen.getByText('Discover');
+    
+    userEvent.click(discoverNav);
+
+    expect(history.location.pathname).toBe('/discover');
+  });
+
+  it('should link to home when \'Saved\' is clicked', () => {
+    const savedNav = screen.getByText('Saved');
+    
+    userEvent.click(savedNav);
+
+    expect(history.location.pathname).toBe('/saved');
   });
 });
