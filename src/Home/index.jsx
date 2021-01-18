67a560a99e144f3ee785fe44db6fa132011fec9f
Kara Caputo <kncaputo@gmail.com>
2021-01-18 15:38:13 -0500

    Add background img to home and text

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 
   return (
-    <main className='home-main'>
+    <main className='home-main' style={{backgroundImage: `url(${background})`}}>
       <h1>Welcome to Urban Stargazer</h1>
+      <p>Humans have stared at the starry sky for mellenia. It </p>
       <Link to='/discover'> 
         <button>Click for random image</button>
       </Link>  
