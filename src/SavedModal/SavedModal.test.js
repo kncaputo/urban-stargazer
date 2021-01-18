6b3002b6115b0c6b9bf96267b1a1d4a0111ca72a
Kara Caputo <kncaputo@gmail.com>
2021-01-18 10:17:35 -0500

    Add initial render tests for SavedModal

--- src/SavedModal/SavedModal.test.js
+++ src/SavedModal/SavedModal.test.js
@@ -1,7 +1,45 @@
-
+import SavedModal from './index.jsx';
 import { act, render, screen, fireEvent } from '@testing-library/react';
-import Saved from './index.jsx';
-import { saveToLocalStorage } from '../utilities';
-import { image1, image2, filteredImage1, filteredImage2 } from '../sampleData';
 import '@testing-library/jest-dom';
-jest.mock('../utilities');
+import { filteredImage1, filteredImage2 } from '../sampleData';
+
+describe('SavedModal', () => {
+  const mockCloseModal = jest.fn();
+  const mockDeleteSavedImage = jest.fn();
+
+  beforeEach(() => {
+    render(
+      <SavedModal 
+        displayImage={filteredImage1}
+        closeModal={mockCloseModal}
+        deleteSavedImage={mockDeleteSavedImage}
+      />
+    );
+  });
+
+  it('should have an image', () => {
+    const img = screen.getByAltText('Jets from Unusual Galaxy Centaurus A');
+  
+    expect(img).toBeInTheDocument();
+  });
+
+  it('should have a title', () => {
+    const title = screen.getByText('Jets from Unusual Galaxy Centaurus A');
+  
+    expect(title).toBeInTheDocument();
+  });
+
+  it('should have a date', () => {
+    const date = screen.getByText('2021-01-17');
+  
+    expect(date).toBeInTheDocument();
+  });
+
+  it('should have an explanation', () => {
+    const explanation = screen.getByText('The jets are over a million light years long.');
+  
+    expect(explanation).toBeInTheDocument();
+  });
+
+
+});
