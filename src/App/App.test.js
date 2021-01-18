9eaf5f53e2d10541792728825916a801105ca497
Kara Caputo <kncaputo@gmail.com>
2021-01-18 13:03:51 -0500

    Add render test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,37 @@
 import App from './index.jsx';
 import { act, render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { fetchPictureFromDate } from '../apiCalls';
+import { MemoryRouter } from 'react-router-dom';
+import { fetchPictureFromDate } from '../apiCalls/apiCalls';
 import { image1, image2 } from '../sampleData';
-import { filterData, saveToLocalStorage } from '../utilities';
-jest.mock('../apiCalls');
+import { filterData, saveToLocalStorage } from '../utilities/utilities';
+jest.mock('../apiCalls/apiCalls');
+
+describe('App', () => {
+
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>  
+    );
+  });
+
+  it('should render correctly ', () => {
+    //header
+    const pageTitle = screen.getByText('Urban Stargazer');
+    const discover = screen.getByText('Discover');
+    const saved = screen.getByText('Saved');
+
+    //footer
+    const footerText = screen.getByText('Photos brought to you by NASA\'s Astronomy Picture of the Day archive');
+    const footerLogo = screen.getByAltText('NASA logo');
+
+    expect(pageTitle).toBeInTheDocument();
+    expect(discover).toBeInTheDocument();
+    expect(saved).toBeInTheDocument();
+
+    expect(footerText).toBeInTheDocument();
+    expect(footerLogo).toBeInTheDocument();
+  });
+});
