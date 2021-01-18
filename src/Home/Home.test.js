c6c570617c117fe9c0eadb7d6d5626b3f89f2f24
Kara Caputo <kncaputo@gmail.com>
2021-01-18 18:25:25 -0500

    Add home test outline

--- src/Home/Home.test.js
+++ src/Home/Home.test.js
@@ -1,11 +1,14 @@
 import Home from './index.jsx';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Home', () => {
   it('should render correctly', () => {
     render(
-      <Home />
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>  
     );
     
     expect().toBeInTheDocument();
