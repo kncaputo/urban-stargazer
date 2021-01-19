d6718c2e5b1e74c4c61db46cf0ce67996262e583
Kara Caputo <kncaputo@gmail.com>
2021-01-18 19:06:43 -0500

    Add styles and flex to Home; make responsive

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react';
+import React from 'react';
 import background from '../assets/hubble-stars.jpg'
 import { Link } from 'react-router-dom';
 import './Home.scss'
@@ -7,22 +7,24 @@ const Home = () => {
 
   return (
     <main className='home-main' style={{backgroundImage: `url(${background})`}}>
-      <h1>Welcome to Urban Stargazer</h1>
+      <h1 id='home-title'>Welcome to Urban Stargazer</h1>
 
       <section className='box'>
         <section className='aside'></section>
-        <p className='center'>
-          Humans have stared at the starry sky and contemplated the cosmos for millennia. 
-          It is an essential part of human nature. 
-          But with 80% of the world's population now living under a light polluted sky (darksky.org), 
-          and with increasing numbers of satellites being put into orbit for telecommunications, 
-          our night sky is dramatically changing.
-        </p>
+        <section className='center'>
+          <p className='center-text'>
+            Humans have stared at the starry sky and contemplated the cosmos for millennia. 
+            It is an essential part of human nature. 
+            But with 80% of the world's population now living under a light polluted sky (darksky.org), 
+            and with increasing numbers of satellites being put into orbit for telecommunications, 
+            our night sky is dramatically changing.
+          </p>
+        </section>
         <section className='aside'></section>
       </section>
 
       <Link to='/discover'> 
-        <button>Click for random image</button>
+        <button id='explore-cosmos-button'>Click to explore the cosmos</button>
       </Link>  
     </main>
   )
