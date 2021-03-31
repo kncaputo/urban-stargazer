08b929dee74ff5a48fecf56f4cc42211e563a0b1
Kara Caputo <kncaputo@gmail.com>
2021-03-31 09:10:03 -0400

    Add home content wrapper

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -7,25 +7,27 @@ const Home = () => {
 
   return (
     <main data-testid='home' className='home-main' style={{backgroundImage: `url(${background})`}}>
-      <h1 id='home-title' data-testid='home-title'>Welcome to Urban Stargazer</h1>
+      <section className='home-content-wrapper'>
+        <h1 id='home-title' data-testid='home-title'>Welcome to Urban Stargazer</h1>
 
-      <section className='box'>
-        <section className='aside'></section>
-        <section className='center'>
-          <p className='center-text' data-testid='home-text'>
-          Humans have stared at the starry sky and contemplated the cosmos for millennia. 
-          It's an inextricable part of human nature. But with 80% of the world's population 
-          now living under a light polluted sky (darksky.org), and with increasing numbers 
-          of satellites being put into orbit for telecommunications, our night sky is 
-          dramatically changing. 
-          </p>
+        <section className='box'>
+          <section className='aside'></section>
+          <section className='center'>
+            <p className='center-text' data-testid='home-text'>
+            Humans have stared at the starry sky and contemplated the cosmos for millennia. 
+            It's an inextricable part of human nature. But with 80% of the world's population 
+            now living under a light polluted sky (darksky.org), and with increasing numbers 
+            of satellites being put into orbit for telecommunications, our night sky is 
+            dramatically changing. 
+            </p>
+          </section>
+          <section className='aside'></section>
         </section>
-        <section className='aside'></section>
-      </section>
 
-      <Link to='/discover'> 
-        <button id='explore-cosmos-button'>Click to explore the cosmos</button>
-      </Link>  
+        <Link to='/discover'> 
+          <button id='explore-cosmos-button'>Click to explore the cosmos</button>
+        </Link>  
+      </section>
     </main>
   )
 }
