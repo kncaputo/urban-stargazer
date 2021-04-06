66e5ca750cedd5d740af1d00fc97e9ff94518f84
Kara Caputo <kncaputo@gmail.com>
2021-04-06 09:00:03 -0400

    Add styles to background image

--- src/Error/index.js
+++ src/Error/index.js
@@ -1,10 +1,11 @@
 import freeflyer from './freeflyer.jpeg';
 import './Error.scss';
 
+
 const Error = () => {
   return(
-    <main id='error-main' style={{backgroundImage: `url(${freeflyer})`}}>
-      <p>Sorry, we've encountered an error. Please go back.</p>
+    <main id='error-main' style={{backgroundImage: `url(${freeflyer})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
+      <p id='error-text'>Houston, we've got a problem. Please go back.</p>
     </main>
   )
 }
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY
 const endpoint = 'https://api.nasa.gov/planetary/apod';
 
 export const fetchPictureFromDate = (date) => {
-  return fetch(`${endpoint}?api_key=${NASA_API_KEY}&date=${date}`)
+  return fetch(`${endpoint}?api_key=${NASA_API_KEY}&dahte=${date}`)
   .then(response => {
     if (!response.ok) {
       throw Error
